c88f19d15df5351a5307db0f15aac7db1ec74831
Vojtech Suchanek <suchavo3@cvut.cz>
2024-09-13 15:07:51 +0200

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,48 @@
-// ./src/middleware.ts
+// // ./src/middleware.ts
 
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
+// import { createClient } from "@/prismicio";
+
+// export async function middleware(request: NextRequest) {
+//   const client = createClient();
+//   const repository = await client.getRepository();
+
+//   const locales = repository.languages.map((lang) => lang.id);
+//   const defaultLocale = locales[0];
+
+//   // Check if there is any supported locale in the pathname
+//   const { pathname } = request.nextUrl;
+
+//   const pathnameIsMissingLocale = locales.every(
+//     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+//   );
+
+//   // Redirect to default locale if there is no supported locale prefix
+//   if (pathnameIsMissingLocale) {
+//     return NextResponse.rewrite(
+//       new URL(`/${defaultLocale}${pathname}`, request.url)
+//     );
+//   }
+// }
+
+// export const config = {
+//   matcher: ["/((?!_next|images).*)", "/"],
+// };
+
+import { createLocaleRedirect } from "@prismicio/next";
 import { createClient } from "@/prismicio";
+import { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   const client = createClient();
-  const repository = await client.getRepository();
+  const redirect = await createLocaleRedirect({ client, request });
 
-  const locales = repository.languages.map((lang) => lang.id);
-  const defaultLocale = locales[0];
-
-  // Check if there is any supported locale in the pathname
-  const { pathname } = request.nextUrl;
-
-  const pathnameIsMissingLocale = locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  );
-
-  // Redirect to default locale if there is no supported locale prefix
-  if (pathnameIsMissingLocale) {
-    return NextResponse.rewrite(
-      new URL(`/${defaultLocale}${pathname}`, request.url)
-    );
+  if (redirect) {
+    return redirect;
   }
 }
 
 export const config = {
-  matcher: ["/((?!_next|images).*)", "/"],
+  // Do not localize these paths
+  matcher: ["/((?!_next|api|slice-simulator|icon.svg).*)"],
 };
