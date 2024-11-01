510d89a7a1809bbf9a4cb47fb9b9d3964b80b4e4
Vojtech Suchanek <cenenaci.cz@gmail.com>
2024-11-01 14:36:33 +0100

    idk

--- src/app/[lang]/slice-simulator/page.tsx
+++ src/app/[lang]/slice-simulator/page.tsx
@@ -8,17 +8,17 @@ import { redirect } from "next/navigation";
 
 import { components } from "@/slices";
 
-export default function SliceSimulatorPage({
+export default async function SliceSimulatorPage({
   searchParams,
 }: SliceSimulatorParams & { searchParams: { secret?: string } }) {
+  const { state } = await searchParams;
   if (
     process.env.SLICE_SIMULATOR_SECRET &&
     searchParams.secret !== process.env.SLICE_SIMULATOR_SECRET
   ) {
     redirect("/");
   }
-
-  const slices = getSlices(searchParams.state);
+  const slices = getSlices(state);
 
   return (
     <SliceSimulator>
