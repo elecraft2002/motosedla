f56c6e20148f3385f414cf2deba58bddce907534
Vojtech Suchanek <cenenaci.cz@gmail.com>
2024-11-05 11:00:32 +0100

    bugfix

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -101,7 +101,7 @@ export default function Search({
                     className="hover:bg-slate-200/10 transition-all"
                   >
                     <Link
-                      href={`${lang}/seat/${result.uid}`}
+                      href={`/${lang}/seat/${result.uid}`}
                       className="flex items-center gap-2"
                       onClick={() => {
                         setShownSearchState(false);
