9dedb770bca31e877a60b7a8468d9b6fe285327e
Vojtech Suchanek <cenenaci.cz@gmail.com>
2024-11-05 11:02:58 +0100

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'motosedla.vercel.app',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
