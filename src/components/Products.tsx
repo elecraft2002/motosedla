629129b1368fbbec601dcaa34319fc86ad8b8dba
Vojtech Suchanek <cenenaci.cz@gmail.com>
2025-02-22 20:26:20 +0100

    redeploy

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -17,7 +17,7 @@ export default function Products({
   const offset = 20;
   const [len, setLen] = useState(offset);
   return (
-    <AnimatePresence>
+     <AnimatePresence>
       <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {products.slice(0, len).map((product, i) => {
           // if (i >= len) return null;
