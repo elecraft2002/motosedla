f1edc3ca712d57747d42a6183d2079a9a540121e
Vojtech Suchanek <cenenaci.cz@gmail.com>
2026-02-19 10:03:17 +0100

    force

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
-        hostname: '*',
+        hostname: "*",
       },
     ],
   },
